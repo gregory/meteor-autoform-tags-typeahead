109322e4abbb2e76e6e51fe4f5144f128760d5b5
gregory <greg2502@gmail.com>
2016-10-04 15:53:01 -0700

    bump version to fix bug

--- package.js
+++ package.js
@@ -14,7 +14,7 @@ Package.onUse(function (api) {
         'aldeed:simple-schema@1.3.3',
         'aldeed:collection2@2.5.0',
         'aldeed:autoform@5.5.0',
-        'ajduke:bootstrap-tagsinput@0.7.0',
+        'ajduke:bootstrap-tagsinput@0.7.1',
         'mrt:bootstrap3-typeahead@0.1.1',
         'templating'
     ]);
